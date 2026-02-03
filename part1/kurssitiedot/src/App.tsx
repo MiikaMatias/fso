69dc51790bdfdf2a14ca8ee53adfaf601f6321b9
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2026-02-03 15:17:23 +0200

    1.1

--- part1/kurssitiedot/src/App.tsx
+++ part1/kurssitiedot/src/App.tsx
@@ -1,10 +1,36 @@
 
-const Total = () => {
-  
+type TotalProps = {
+  amount: number
+};
+const Total = ({amount}: TotalProps) => {
+  return(
+    <>
+      <p>Number of exercises: {amount}</p>
+    </>
+  )
 }
 
-const Content = () => {
-
+type Part = {
+  name: string;
+  exercises: number;
+};
+type ContentProps = {
+  parts: Array<Part>
+};
+const Content = ({ parts }: ContentProps) => {
+  return (
+    <>
+      <p>
+        {parts[0].name} {parts[0].exercises}
+      </p>
+      <p>
+        {parts[1].name} {parts[1].exercises}
+      </p>
+      <p>
+        {parts[2].name} {parts[2].exercises}
+      </p>
+    </>
+  )
 }
 
 type HeaderProps = {
@@ -19,26 +45,28 @@ const Header = (props: HeaderProps) => {
 }
 
 const App = () => {
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts: Array<Part> = [
+    {
+      name:'Fundamentals of React', 
+      exercises:10
+    }, 
+    {
+      name:'Using props to pass data',
+      exercises:7
+    }, 
+    {
+      name:'State of a component',
+      exercises:14
+    }
+  ];
+
+
 
   return (
     <div>
       <Header course='Half Stack application development'></Header>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content parts={parts}></Content>
+      <Total amount={parts.length}></Total>
     </div>
   )
 }
