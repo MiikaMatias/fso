c20ef9e208319f20db5b03bf4bcd5e9e098a2652
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2026-02-03 14:55:26 +0200

    header

--- part1/kurssitiedot/src/App.tsx
+++ part1/kurssitiedot/src/App.tsx
@@ -1,4 +1,17 @@
-function App() {
+
+const Total = () => {
+  
+}
+
+const Content = () => {
+
+}
+
+const Header = () => {
+
+}
+
+const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
   const exercises1 = 10
